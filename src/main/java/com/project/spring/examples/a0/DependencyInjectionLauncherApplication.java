0273a951a48347d50134251b218ed77ce1506a37
ASHOK E <ashoke@ASHOKs-Air.alphionsee.in>
2023-09-18 13:03:01 +0530

    Constructor Injection

--- src/main/java/com/project/spring/examples/a0/DependencyInjectionLauncherApplication.java
+++ src/main/java/com/project/spring/examples/a0/DependencyInjectionLauncherApplication.java
@@ -15,20 +15,28 @@ import java.util.Arrays;
 class YourBusinessClass{
 
     Dependency1 dependency1;
-
     Dependency2 dependency2;
 
     @Autowired
-    public void setDependency1(Dependency1 dependency1) {
-        System.out.println("Setter Injection -  setDependency1");
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        System.out.println("Constructor injection");
         this.dependency1 = dependency1;
-    }
-
-    @Autowired
-    public void setDependency2(Dependency2 dependency2) {
-        System.out.println("Setter Injection - setDependency2");
         this.dependency2 = dependency2;
     }
+
+
+
+//    @Autowired
+//    public void setDependency1(Dependency1 dependency1) {
+//        System.out.println("Setter Injection -  setDependency1");
+//        this.dependency1 = dependency1;
+//    }
+//
+//    @Autowired
+//    public void setDependency2(Dependency2 dependency2) {
+//        System.out.println("Setter Injection - setDependency2");
+//        this.dependency2 = dependency2;
+//    }
 
     public String toString(){
         return "Using "+ dependency1 + " and " + dependency2;
