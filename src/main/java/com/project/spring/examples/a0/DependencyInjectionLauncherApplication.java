597ba79ab7d46618f5248b354b054bc4c4d2b2d3
ASHOK E <ashoke@ASHOKs-Air.alphionsee.in>
2023-09-18 13:01:34 +0530

    Setter Injection

--- src/main/java/com/project/spring/examples/a0/DependencyInjectionLauncherApplication.java
+++ src/main/java/com/project/spring/examples/a0/DependencyInjectionLauncherApplication.java
@@ -13,10 +13,22 @@ import java.util.Arrays;
 //Dependency2
 @Component
 class YourBusinessClass{
-    @Autowired
+
     Dependency1 dependency1;
-    @Autowired
+
     Dependency2 dependency2;
+
+    @Autowired
+    public void setDependency1(Dependency1 dependency1) {
+        System.out.println("Setter Injection -  setDependency1");
+        this.dependency1 = dependency1;
+    }
+
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        System.out.println("Setter Injection - setDependency2");
+        this.dependency2 = dependency2;
+    }
 
     public String toString(){
         return "Using "+ dependency1 + " and " + dependency2;
